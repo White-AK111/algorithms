d86181dca7cbf908f400f30a084ec954afe59e17
White-AK111 <formatmyc@gmail.com>
2021-10-27 09:15:09 -0700

    duplicates

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/White-AK111/algorithms/duplicates"
+	"github.com/White-AK111/algorithms/stones"
+	"github.com/White-AK111/algorithms/units"
 )
 
 func main() {
-	//stones.GetJewelry()
-	//units.GetMaxUnits()
+	stones.GetJewelry()
+	units.GetMaxUnits()
 	duplicates.DeleteDuplicates()
 }

7e22bf8ff319404e3ecd18fd19cd62a116a1d9fa
White-AK111 <formatmyc@gmail.com>
2024-08-19 01:10:17 -0700

    credit

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/White-AK111/algorithms/credit"
+import (
+	"github.com/White-AK111/algorithms/stones"
+)
 
 func main() {
-	//stones.GetJewelry()
+	stones.GetJewelry()
 	//units.GetMaxUnits()
 	//duplicates.DeleteDuplicates()
 	//brackets.DoBrackets()
 	//anagrams.CheckAnagrams()
 	//travel.FindWay()
-	credit.GetCredit()
+	//credit.GetCredit()
 }
